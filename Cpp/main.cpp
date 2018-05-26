fd114eb93a4a71cc1cb6583c52ac568c3b714e23
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-05-26 18:30:57 +0200

    Update main.cpp

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -24,63 +24,62 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
 #include <iostream>
 #include <iomanip>
-using namespace std;
 
 int main()
 {
 	int r, g, b, a, dec;
 	float fr, fg, fb, fa;
 
-	cout << "RGBA to Float Color Converter"<< endl << endl;
-	cout << "Please enter RGBA values in range [0-255]" << endl;
+	std::cout << "RGBA to Float Color Converter\n" << std::endl;
+	std::cout << "Please enter RGBA values in range [0-255]" << std::endl;
 
-	cout << "[R]Red: ";
-	cin >> r;
+	std::cout << "[R]Red: ";
+	std::cin >> r;
 	while (r > 255 || r < 0) { 
-		cout << "(!) Please enter valid number in range [0-255]" << endl << "[R]Red:";
-		cin >> r;
+		std::cout << "(!) Please enter valid number in range [0-255]" << std::endl << "[R]Red:";
+		std::cin >> r;
 	}
 
-	cout << "[G]Green: ";
-	cin >> g;
+	std::cout << "[G]Green: ";
+	std::cin >> g;
 	while (g > 255 || g < 0) {
-		cout << "(!) Please enter valid number in range [0-255]" << endl << "[G] Green:";
-		cin >> g;
+		std::cout << "(!) Please enter valid number in range [0-255]" << std::endl << "[G] Green:";
+		std::cin >> g;
 	}
 
-	cout << "[B]Blue: ";
-	cin >> b;
+	std::cout << "[B]Blue: ";
+	std::cin >> b;
 	while (b > 255 || b < 0) {
-		cout << "(!) Please enter valid number in range [0-255]" << endl << "[B] Blue:";
-		cin >> b;
+		std::cout << "(!) Please enter valid number in range [0-255]" << std::endl << "[B] Blue:";
+		std::cin >> b;
 	}
 
-	cout << "[A]Alpha: ";
-	cin >> a;
+	std::cout << "[A]Alpha: ";
+	std::cin >> a;
 	while (a > 255 || a < 0) {
-		cout << "(!) Please enter valid number in range [0-255]" << endl << "[A] Alpha:";
-		cin >> a;
+		std::cout << "(!) Please enter valid number in range [0-255]" << std::endl << "[A] Alpha:";
+		std::cin >> a;
 	}
 
-	cout << "Please enter number of decimal places of float values: ";
-	cin >> dec;
+	std::cout << "Please enter number of decimal places of float values: ";
+	std::cin >> dec;
 
 	fr = (float)r / 255;
 	fg = (float)g / 255;
 	fb = (float)b / 255;
 	fa = (float)a / 255;
 
-	cout << endl;
+	std::cout << std::endl;
 
-	cout << "Converted values RGBA -> Float:" << endl;
-	cout << fixed;
-	cout << setprecision(dec);
-	cout << "[R]Red: " << fr << "f" << endl;
-	cout << "[G]Green: " << fg << "f" << endl;
-	cout << "[B]Blue: " << fb << "f" << endl;
-	cout << "[A]Alpha: " << fa << "f" << endl << endl;
+	std::cout << "Converted values RGBA -> Float:" << std::endl;
+	std::cout << std::fixed;
+	std::cout << std::setprecision(dec);
+	std::cout << "[R]Red: " << fr << "f" << std::endl;
+	std::cout << "[G]Green: " << fg << "f" << std::endl;
+	std::cout << "[B]Blue: " << fb << "f" << std::endl;
+	std::cout << "[A]Alpha: " << fa << "f" << std::endl;
 
-	cout << "[RGBA]: " << fr << "f, " << fg << "f, " << fb << "f, " << fa << "f" << endl;
+	std::cout << "\n[RGBA]: " << fr << "f, " << fg << "f, " << fb << "f, " << fa << "f" << std::endl;
 
 	return 0;
 }
